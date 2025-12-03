b62726aff7ff01438d3fe6a69cadc1c1e2f27d33
Amitoshdeep <amitosh1999@gmail.com>
2025-12-03 08:58:56 +0530

    coffe update

--- src/components/ui/BuyMeCoffeeButton.jsx
+++ src/components/ui/BuyMeCoffeeButton.jsx
@@ -3,7 +3,7 @@ import { Heart, X } from "lucide-react";
 import QRCode from "qrcode";
 
 function BuyMeCoffeeButton() {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const [qrSrc, setQrSrc] = useState("");
 
   const UPI_ID = "amitosh1999@ptyes";
