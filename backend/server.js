99d92ade89ec45bec83b62895b5d3f59d7c96716
Amitoshdeep <amitosh1999@gmail.com>
2026-01-10 11:42:43 +0530

    cors: Added new domain to CORS

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://arc-lane.vercel.app",
-    "https://arclane.deepamitosh.online"
+    "https://arclane.amitosh.site"
   ],
   credentials: true
 }));
